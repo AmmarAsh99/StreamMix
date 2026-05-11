b9a0d48ad1613c771769b0ec20c18542bf70dc65
Ammar <outseccoc@hotmail.com>
2026-05-12 01:03:56 +0300

    api

--- frontend/stream-client/src/app/api/user.service.ts
+++ frontend/stream-client/src/app/api/user.service.ts
@@ -9,6 +9,6 @@ export class UserService {
   constructor(private http: HttpClient) {}
   private backend = environment.apiUrl;
   createUser(credentials: { email: string; password: string }) {
-    return this.http.post(`${this.backend}/user`, credentials);
+    return this.http.post(`${this.backend}user`, credentials);
   }
 }
